8f062a08c4f8e9a521e61f64db83f08380263fc9
Oliver <oliver.ferrari20@gmail.com>
2026-03-20 16:32:18 +0100

    successfully integrated the ros2 event publisher node with the new zmq based communication pipeline for the sensor on the Pi OS

--- sensor_system/sensor_system/event_publisher_node.py
+++ sensor_system/sensor_system/event_publisher_node.py
@@ -11,7 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import cv2
+import numpy as np
+import os
+import time
+from sensor.cameras.camera_packages.zmq_Communication import Subscriber
 import rclpy
 from rclpy.node import Node
 
@@ -26,12 +30,15 @@ class EventPublisher(Node):
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.reciever = Subscriber()
 
     def timer_callback(self):
+        
+        message = self.reciever.recv_array()
         msg = String()
         msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % message[1][0])
         self.i += 1
 
 
